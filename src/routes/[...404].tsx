2c9460d3d9712481a4c9eaf701c785ac381ababf
kruckenberg <nkruckenberg@gmail.com>
2025-06-27 07:12:53 -0500

    add fira code font

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -3,8 +3,8 @@ import { A } from "@solidjs/router";
 export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
+      <h1 class="max-6-xs text-6xl text-sky-700 my-16">
+        not found
       </h1>
       <p class="my-4">
         <A href="/" class="text-sky-600 hover:underline">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+      <h1 class="max-6-xs text-6xl text-sky-700 my-16">
         source cod
       </h1>
     </main>
