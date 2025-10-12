158778a05eeec8711a2fdea9ed19d8eb8056231c
kruckenberg <nkruckenberg@gmail.com>
2025-10-12 01:44:11 -0500

    pass schema to db instance for type inference, query constructor

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,3 +1,4 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { schema } from "./schema/index.ts";
 
-export const db = drizzle(Deno.env.get("DATABASE_URL")!);
+export const db = drizzle(Deno.env.get("DATABASE_URL")!, { schema });
