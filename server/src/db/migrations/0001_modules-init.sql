60f7e2aa81af6e07d3ec9115f9f5595d73b3ed29
kruckenberg <nkruckenberg@gmail.com>
2025-10-12 01:43:25 -0500

    recreate modules migration

--- server/src/db/migrations/0001_modules-init.sql
+++ server/src/db/migrations/0001_modules-init.sql
@@ -1,15 +1,5 @@
 CREATE SCHEMA "modules";
 --> statement-breakpoint
-CREATE TABLE "modules"."files" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" text NOT NULL,
-	"content" text NOT NULL,
-	"content_hash" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "files_content_hash_unique" UNIQUE("content_hash")
-);
---> statement-breakpoint
 CREATE TABLE "modules"."module_to_file" (
 	"module_id" uuid NOT NULL,
 	"file_id" uuid NOT NULL,
@@ -29,9 +19,25 @@ CREATE TABLE "modules"."modules" (
 	CONSTRAINT "modules_slug_unique" UNIQUE("slug")
 );
 --> statement-breakpoint
-CREATE TABLE "modules"."user_modules" (
+CREATE TABLE "modules"."starter_files" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "modules"."user_files" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" text NOT NULL,
+	"name" text NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "modules"."user_modules" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"module_id" uuid NOT NULL,
 	"files" jsonb NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
@@ -39,6 +45,6 @@ CREATE TABLE "modules"."user_modules" (
 );
 --> statement-breakpoint
 ALTER TABLE "modules"."module_to_file" ADD CONSTRAINT "module_to_file_module_id_modules_id_fk" FOREIGN KEY ("module_id") REFERENCES "modules"."modules"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "modules"."module_to_file" ADD CONSTRAINT "module_to_file_file_id_files_id_fk" FOREIGN KEY ("file_id") REFERENCES "modules"."files"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "modules"."user_modules" ADD CONSTRAINT "user_modules_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "modules"."module_to_file" ADD CONSTRAINT "module_to_file_file_id_starter_files_id_fk" FOREIGN KEY ("file_id") REFERENCES "modules"."starter_files"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "modules"."user_files" ADD CONSTRAINT "user_files_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "modules"."user_modules" ADD CONSTRAINT "user_modules_module_id_modules_id_fk" FOREIGN KEY ("module_id") REFERENCES "modules"."modules"("id") ON DELETE cascade ON UPDATE no action;
