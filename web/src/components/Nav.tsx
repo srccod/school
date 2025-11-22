6eadd20812fadf8527d9ae960654b05a34a89d47
kruckenberg <nkruckenberg@gmail.com>
2025-11-22 13:01:18 -0600

    use dropdown instead of nav menu to get submenu

--- web/src/components/Nav.tsx
+++ web/src/components/Nav.tsx
@@ -1,18 +1,22 @@
-import { createResource, For, Show } from "solid-js";
 import { useNavigate } from "@solidjs/router";
-import { useUser } from "../stores/user.tsx";
-import { Button } from "./ui/Button.tsx";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuTrigger,
-} from "./ui/NavigationMenu.tsx";
+import { createResource, For, Show } from "solid-js";
+import type { ModuleResponse } from "../../../server/src/shared-types.ts";
 import logo from "../../assets/srccod-logo-sm.webp";
 import { getModules } from "../lib/apiClient.ts";
+import { useUser } from "../stores/user.tsx";
 import { HamburgerMenuIcon } from "./icons.tsx";
-import type { ModuleResponse } from "../../../server/src/shared-types.ts";
+import { Button } from "./ui/Button.tsx";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/DropdownMenu.tsx";
 
 export default function Nav() {
   const { user, logout } = useUser();
@@ -25,37 +29,45 @@ export default function Nav() {
       <div class="flex items-center gap-1">
         <img src={logo} alt="srccod Logo" class="h-12" />
       </div>
-      <div class="flex">
+      <div class="flex align-content-center items-center gap-4">
         <Show when={user.isLoggedIn}>
-          <NavigationMenu>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>
+          <span>{user.username}</span>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button variant="ghost">
                 <HamburgerMenuIcon />
-              </NavigationMenuTrigger>
-              <NavigationMenuContent class="w-56 overflow-y-auto max-h-96">
-                <For
-                  each={modules()?.sort((a, b) => a.name.localeCompare(b.name))}
-                >
-                  {(module) => (
-                    <NavigationMenuLink
-                      class="text-sm"
-                      onClick={() => navigate(`/${module.slug}`)}
-                    >
-                      {module.name}
-                    </NavigationMenuLink>
-                  )}
-                </For>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Button variant="ghost" onClick={() => logout(navigate)}>
-                log out
               </Button>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <span>{user.username}</span>
-            </NavigationMenuItem>
-          </NavigationMenu>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent class="w-56">
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>
+                  modules
+                </DropdownMenuSubTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuSubContent class="max-h-96 overflow-y-auto">
+                    <For
+                      each={modules()?.sort((a, b) =>
+                        a.name.localeCompare(b.name)
+                      )}
+                    >
+                      {(module) => (
+                        <DropdownMenuItem
+                          class="text-sm"
+                          onClick={() => navigate(`/${module.slug}`)}
+                        >
+                          {module.name}
+                        </DropdownMenuItem>
+                      )}
+                    </For>
+                  </DropdownMenuSubContent>
+                </DropdownMenuPortal>
+              </DropdownMenuSub>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => logout(navigate)}>
+                log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </Show>
       </div>
     </div>
